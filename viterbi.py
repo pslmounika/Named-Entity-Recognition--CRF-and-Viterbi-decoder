d992d7a426bba5c7bb69d6db5095f75f67108697
pslmounika <pslmounika@gmail.com>
2018-04-02 10:42:49 -0700

    Update viterbi.py

--- viterbi.py
+++ viterbi.py
@@ -1,19 +1,7 @@
 import numpy as np
 
 def run_viterbi(emission_scores, trans_scores, start_scores, end_scores):
-    #f = open('viterbi', 'w')
-    #N is number of tags and L is number of words
-    #print "Inside run_viterbi"
-    #print "Emission "+str(emission_scores)
-    #print"++++"+str(isinstance(emission_scores,np.ndarray))
-    #print "Trans "+str(trans_scores)
-    #print "Start "+str(start_scores)
-    #print "End "+str(end_scores)
-    #f.write("Emission Scores: \n"+str(emission_scores))
-    #f.write("Transmission Scores: \n"+str(trans_scores))
-    #f.write("Start scores: \n"+str(start_scores))
-    #f.write("End scores: \n"+str(end_scores))
-    """
+   
     #Transmission prob is P(currentTag/PreviousTag)
     #Emission prob is P(currentWord/currentTag)
     
@@ -57,7 +45,7 @@ def run_viterbi(emission_scores, trans_scores, start_scores, end_scores):
     score=0
     y = []
     for i in xrange(N):
-        # stupid sequence
+       
         y.append(i % L)
     # score set to 0
     LastColScore=[]
@@ -69,9 +57,7 @@ def run_viterbi(emission_scores, trans_scores, start_scores, end_scores):
     backpointers=[]
 
 
-    #N=3
-    #L=4
-    #print emission_scores[0,1]
+    
     PwordTag =0
     cellMax=[]
     cellMaxTuple=[]
@@ -79,95 +65,66 @@ def run_viterbi(emission_scores, trans_scores, start_scores, end_scores):
     ColumnMaxTuple=[]
     result=[]
     resultTuple=[]
-    #print start_scores[0]
     col=N
     for i in xrange(col+1):
-        #print i
-        #xprint "Emission : "+str(emission_scores[0])
         ColumnMaxTuple=[]
         if(i==0):
             for j in range(0,L):
                 #print "j: "+str(j)
                 if(i==0 and j<L):
-                    #i=0
-                    #print start_scores[j]
-                    #print "++++"+str(emission_scores[i,j])
+                  
                     val=start_scores[j]+emission_scores[i,j]
                     scoreMatrix[j,i]=(val, j)
                     ColumnMaxTuple.append((scoreMatrix[j,i],j))
 
-            #print "Score matrix: \n"+str(scoreMatrix)
-            #print "ColumnMatrix: "+str(ColumnMaxTuple)
+
             ColumnMaxTuple.sort(key=lambda tup: tup[0], reverse=True)
-            #resultTuple.append(ColumnMaxTuple[0])
-            #print "resultTuple: "+str(resultTuple)
+
         elif(i==N):
-            #print"Score matrix until now: \n"+str(scoreMatrix)
-            #print "col: "+str(col)
-            #print "i: "+str(i)
-            #print "end score: "+str(end_scores)
-            #PwordTag = end_scores[N-1, 0]
-            #print"Last PwordTag: "+str(PwordTag)
+            
             for j in range(0,L):
                 if (j < L):
                     val = end_scores[j] + scoreMatrix[j, i-1][0]
                     scoreMatrix[j, i] =(val,j)
                     #print"scoree-->"+str(scoreMatrix[j,i])
                     ColumnMaxTuple.append(scoreMatrix[j, i])
-            #print "ColumnMatrix: " + str(ColumnMaxTuple)
+            
             ColumnMaxTuple.sort(key=lambda tup: tup[0], reverse=True)
             resultTuple.append(ColumnMaxTuple[0])
-            #print "resultTuple: " + str(resultTuple)
+            
 
 
         else:
 
             for j in range(0,L):
-                #print"***************"+str(j)
+                
                 PwordTag=emission_scores[i,j]
-                #print"PwordTag: "+str(PwordTag)
+                
                 cellMax=[]
 
                 for k in range(0,L):
-                    #print "transscore: "+str(trans_scores[k,j])
-                    #print "scoreMatrix[i-1,j]: "+str(scoreMatrix[k,i-1])
-                    #print "scoreMatrix[i-1,j]: ----->" + str(scoreMatrix[k, i - 1][0])
-                    #smVal=scoreMatrix[k, i - 1][0]
-
                     val=scoreMatrix[k,i-1][0]+PwordTag+trans_scores[k,j]
-                    #print "val: "+str(val)
+                    
                     cellMax.append((val,k))
-                #print "cellMax: "+str(cellMax)
+                
                 cellMax.sort(key=lambda tup: tup[0], reverse=True)
-                #print "cellMax Sorted:   "+"i: "+str(i)+"j: "+str(j)+"\n"+str(cellMax)
-                #print "^^^^^^"+str(cellMax[0])
+                
                 ColumnMaxTuple.append(cellMax[0])
-                #print "after appending to column max tuple "+str(ColumnMaxTuple)
+                
                 scoreMatrix[j,i]=cellMax[0]
-            #print "Score Matrix: "+"i: "+str(i)+"j: "+str(j)+"\n"+str(scoreMatrix)
-            #print "cellMax: "+str(cellMax)
-            #print "Column Max: "+str(ColumnMaxTuple)
-            #ColumnMaxTuple.sort(key=lambda tup: tup[0], reverse=True)
-            #print "Column Max after sorted: "+str(ColumnMaxTuple)
-            #print "ColumnMaxTuple[0]: "+str(ColumnMaxTuple[0])
-            #resultTuple.append(ColumnMaxTuple[0])
-            #print "resultTuple:"+str(resultTuple)
+            
 
-    #print "SCORE MATRIX"
-    #print scoreMatrix
-    #print "result max tuple: "+str(resultTuple)
-    #print "Initial J:"+str(resultTuple[0][1])
+    
     bp=resultTuple[0][1]
     for i in range(N,-1,-1):
-        #print "i: "+str(i)
-        #print "bp: "+str(bp)
+        
         val=scoreMatrix[bp,i]
-        #print "val " + str(val)
+        
         if(i!=N-1):
             backpointers.insert(0,bp)
         bp=val[1]
 
-    #print "Backpointers: "+str(backpointers)
+ 
 
 
 
